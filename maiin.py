0f95fe0958ae0ba5315ae32ad26222f01224347e
Твоё Имя <rasul.rzaev.2014@mail.ru>
2026-06-16 17:29:07 +0800

    добавил игру орел и решка без цикла

--- maiin.py
+++ maiin.py
@@ -2,3 +2,9 @@ print("Тест")
 print("Тест2")
 
 print("Ураа Дима привет")
+
+import random
+input("Нажмите enter чтобы подбросить!")
+coin = random.choice(['Решка','Орёл'])
+
+print("Выпала:", coin)
