e8e67f9850696c3c8b9e517b3da47d310e2d90d4
Твоё Имя <rasul.rzaev.2014@mail.ru>
2026-06-16 17:36:49 +0800

    добавил историю

--- maiin.py
+++ maiin.py
@@ -3,9 +3,14 @@ print("Тест2")
 
 print("Ураа Дима привет")
 
+def add_history(coin):
+    with open('data.txt','a', encoding='utf-8') as file:
+        file.write(f"{coin}\n")
+
 import random
 while True:
     input("Нажмите enter чтобы подбросить!")
     coin = random.choice(['Решка','Орёл'])
+    add_history(coin)
 
     print("Выпала:", coin)
