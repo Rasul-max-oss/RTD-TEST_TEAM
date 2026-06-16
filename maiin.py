72ca0b3d3e61bbc93acbee09dbc9a313c009db57
Твоё Имя <rasul.rzaev.2014@mail.ru>
2026-06-16 17:30:32 +0800

    добавил цикл

--- maiin.py
+++ maiin.py
@@ -4,7 +4,8 @@ print("Тест2")
 print("Ураа Дима привет")
 
 import random
-input("Нажмите enter чтобы подбросить!")
-coin = random.choice(['Решка','Орёл'])
+while True:
+    input("Нажмите enter чтобы подбросить!")
+    coin = random.choice(['Решка','Орёл'])
 
-print("Выпала:", coin)
+    print("Выпала:", coin)
