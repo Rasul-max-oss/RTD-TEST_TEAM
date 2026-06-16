e0a8ffb61feebdf47200d4526a4ad1887e0f2ccd
Твоё Имя <rasul.rzaev.2014@mail.ru>
2026-06-16 17:43:56 +0800

    добавил выбор действия

--- maiin.py
+++ maiin.py
@@ -1,11 +1,21 @@
-print("Тест")
-print("Тест2")
-
-print("Ураа Дима привет")
-
 import random
+def menu():
+    print("1.подбросить монетку")
+    print("2.Посмотреть историю")
+    print("3.Выйти")
+    
+    
 while True:
-    input("Нажмите enter чтобы подбросить!")
-    coin = random.choice(['Решка','Орёл'])
+    menu()
+    choice = input("Выберите (1-3): ")
+    if choice == "1":
+        input("Нажмите enter чтобы подбросить!")
+        coin = random.choice(['Решка','Орёл'])
 
-    print("Выпала:", coin)
+        print("Выпала:", coin)
+    if choice == "2":
+        print("История!")
+    if choice =="3":
+        print("Пока пока")
+        break
+    
